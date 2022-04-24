a1700383cc2b62e773cb4fb57f0086f763adfdd9
emily <eap6787@gmail.com>
2022-04-24 12:25:32 -0600

    create table relationships

--- models/index.js
+++ models/index.js
@@ -3,3 +3,22 @@ const Message = require('./Message');
 const Preferences = require('./Preferences');
 
 // create associations 
+User.hasMany(Message, {
+   foreignKey: 'sender_id',
+   foreignKey: 'recipient_id' 
+});
+
+Message.belongsTo(User, {
+    foreignKey: 'sender_id',
+    foreignKey: 'recipient_id'
+});
+
+User.hasOne(Preferences, {
+    foreignKey: 'user_id'
+});
+
+Preferences.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Message, Preferences };
