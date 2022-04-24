3251fb903e0b993b483dd696b0324cc6d702678e
emily <eap6787@gmail.com>
2022-04-24 14:54:19 -0600

    in progress sign up

--- controllers/api/homeroutes.js
+++ controllers/api/homeroutes.js
@@ -2,3 +2,5 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Message, Preferences } = require('../../models');
 
+
+module.exports = router;
