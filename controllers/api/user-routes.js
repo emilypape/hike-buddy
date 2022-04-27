f4396f3073e67fb9781dac22afe1fc913e02b303
JWViking <julianawilcox@gmail.com>
2022-04-26 20:42:32 -0600

    user login signup signout routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,4 +22,44 @@ router.post('/', (req, res) => {
     });
 });
 
+//login route for the user
+router.post('/login', (req,res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'There is no current user with this email address.'});
+            return;
+        }
+
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.hashed_password);
+        if(!validpassword) {
+            res.status(400).json({message: 'Invalid Password!'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user:dbUserData, message: 'Welcome to Hike Buddy!'});
+        })
+    });
+});
+
+//logout route for the user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+
 module.exports = router;
