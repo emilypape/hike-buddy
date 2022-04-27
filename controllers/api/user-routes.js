232a80eb35c16a40c3bda25b954615205784401b
Carl Wheezer <slushern42@gmail.com>
2022-04-27 15:18:33 -0600

    wrote GET api/user/id in user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,4 +62,38 @@ router.post('/logout', (req, res) => {
     }
   });
 
+// GET /api/users/id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+        { exclude: ['hashed_password']},
+        'first_name',
+        'last_name',
+        'profile_picture',
+        'username',
+        'email'
+        ],
+        include: [
+        {
+            model: Preferences,
+            attributes: ['gender_indentification', 'gender_preference', 'hike_distance', 'hike_pace', 'hike_with_pet', 'hike_climate', 'hike_in_snow', 'water_feature', 'mountain_peak', 'special_equipment']
+        }
+        ],
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json ({ message: 'No user found :( Your perfect match may be hiding somewhere else!'})
+            return;
+        }
+        res.json(dbUserData)
+    })
+    .catch (err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router;
