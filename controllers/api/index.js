40ef41ae51ad597f5ec772bf0402cabac21a8d88
JWViking <julianawilcox@gmail.com>
2022-04-28 17:41:27 -0600

    survey.handlebars beginning

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const messageRoutes = require('./messages');
+const preferenceRoutes = require('./preference-routes.js')
 
 router.use('/users', userRoutes);
 router.use('/messages', messageRoutes);
+router.use('/preferences', preferenceRoutes);
+
 
 module.exports = router;
--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,5 +2,50 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Message, Preferences } = require('../models');
 
+router.get('/preferences/:id', (req, res) => {
+    Preferences.findOne ({
+        where: {
+            id: req.params.user_id
+        },
+        attributes: [
+            'id',
+            'user_id',
+            'gender_identification',
+            'gender_preference',
+            'hike_distance',
+            'hike-pace',
+            'hike_with_pet',
+            'hike_climate',
+            'hike_in_snow',
+            'water_feature',
+            'mountain_peak',
+            'special_equipment',
+            'created_at',
+
+        ],
+        include: [
+        {
+            model: User,
+            attributes:['username', 'first_name', 'last_name', 'profile_picture']
+        }
+        ]
+    })
+    .then(dbPreferencesData => {
+        if (!dbPreferencesData) {
+            res.status(404).json({ message: 'There are no hiker surveys with this id.'});
+            return;
+        }
+        const preferences = dbPreferencesData.get({plain:true});
+        res.render('survey', {
+            preferences,
+            //loggedIn: req.session.loggedIn
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+})
+
 
 module.exports = router;
