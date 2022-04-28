241ddb7413e7f4bc4ee5e6f3e1c63fbf2c0b107b
emily <eap6787@gmail.com>
2022-04-27 19:26:17 -0600

    add get route for message tab

--- controllers/api/messages.js
+++ controllers/api/messages.js
@@ -4,7 +4,40 @@ const sequelize = require('../../config/connection');
 const { User, Message, Preferences } = require('../../models');
 
 // get all conversations under the selected user with a get route
-router.get
+router.get('/:user_id', async (req, res) => {
+    const userId = req.params.user_id;
+    let messages = await Message.findAll({
+        where: {
+            [Op.or]: [
+                { sender_id: userId },
+                { recipient_id: userId },
+            ]
+        }
+    });
+
+    messages = messages.reduce((acc, message) => {
+        acc.add(message.sender_id);
+        acc.add(message.recipient_id);
+        return acc;
+    }, new Set())
+
+    let users = await User.findAll({
+        where: {
+            [Op.or]: Array.from(messages).map((id) => ({id}))
+        }
+    })
+
+    users = users.map((user) => {
+        return {
+            username: user.username,
+            profile_picture: user.profile_picture,
+        }
+    })
+
+    res.json(users)
+    console.log(messages);
+    console.log(users)
+})
 
 // get one conversation between two users with a get route (use grouping?)
 router.get('/:recipient_id/:sender_id', (req, res) => {
