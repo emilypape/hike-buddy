73ae409484caeaf867802455afc3ce0c99ec47b7
JWViking <julianawilcox@gmail.com>
2022-04-28 20:41:18 -0600

    survey route

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,52 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Message, Preferences } = require('../models');
 
-router.get('/preferences/:id', (req, res) => {
-    Preferences.findOne ({
-        where: {
-            id: req.params.user_id
-        },
-        attributes: [
-            'id',
-            'user_id',
-            'gender_identification',
-            'gender_preference',
-            'hike_distance',
-            'hike-pace',
-            'hike_with_pet',
-            'hike_climate',
-            'hike_in_snow',
-            'water_feature',
-            'mountain_peak',
-            'special_equipment',
-            'created_at',
-
-        ],
-        include: [
-        {
-            model: User,
-            attributes:['username', 'first_name', 'last_name', 'profile_picture']
-        }
-        ]
-    })
-    .then(dbPreferencesData => {
-        if (!dbPreferencesData) {
-            res.status(404).json({ message: 'There are no hiker surveys with this id.'});
-            return;
-        }
-        const preferences = dbPreferencesData.get({plain:true});
-        res.render('survey', {
-            preferences,
-            //loggedIn: req.session.loggedIn
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-    });
-})
-
-
 // render profile.handlebars when navigating to /users/:id
 router.get('/users/:id', (req, res) => {
  User.findOne({
@@ -87,4 +41,10 @@ router.get('/users/:id', (req, res) => {
      res.status(500).json(err)
  })
 })
+
+//render survey.handlebars
+router.get('/survey', (req, res) => {
+    res.render('survey')
+});
+
 module.exports = router;
