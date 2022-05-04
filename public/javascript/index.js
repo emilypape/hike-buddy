6256025ee53c6970e4129292088a687cd98f8369
emily <eap6787@gmail.com>
2022-05-04 14:22:27 -0600

    restyle profile page

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,22 @@
-
 const logoutIcon = document.querySelector('#logout-icon');
 const homeFeedIcon = document.querySelector('#home-feed-icon');
 const messageIcon = document.querySelector('#message-icon');
 
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-
-async function logout () {
-    const response = await fetch('/api/users/logout', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' }
-    })
-
-    if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
-async function message () {
-    window.location.assign(`http://localhost:3009/conversation/`)
+function message() {
+  window.location.assign(`http://localhost:3009/conversation/${}`);
 }
 
 messageIcon.addEventListener('click', message);
